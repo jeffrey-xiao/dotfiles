5daf096d784d9523ab2548b98d988c73af7d72d3
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2017-02-25 18:51:28 -0500

    Updated install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ export DOTFILES_DIR
 DOTFILES_DIR=$(pwd)
 
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
-ln -sfv "$DOTFILES_DIR/git/.gitconfig_global" ~
+ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
 ln -sfv "$DOTFILES_DIR/vim/.vimrc" ~
 ln -sfv "$DOTFILES_DIR/vim/.vim" ~
 ln -sfv "$DOTFILES_DIR/system/.bash_aliases" ~
 ln -sfv "$DOTFILES_DIR/system/.bashrc" ~
 ln -sfv "$DOTFILES_DIR/system/.dircolors" ~
-ln -sfv "$DOTFILES_DOR/system/.profile" ~
+ln -sfv "$DOTFILES_DIR/system/.profile" ~

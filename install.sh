4223248e4bcd2739bde9ea379691a75d1284f089
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2017-02-25 19:24:11 -0500

    Updated

--- install.sh
+++ install.sh
@@ -7,9 +7,15 @@ DOTFILES_DIR=$(pwd)
 
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
+
 ln -sfv "$DOTFILES_DIR/vim/.vimrc" ~
 ln -sfv "$DOTFILES_DIR/vim/.vim" ~
+
 ln -sfv "$DOTFILES_DIR/system/.bash_aliases" ~
 ln -sfv "$DOTFILES_DIR/system/.bashrc" ~
 ln -sfv "$DOTFILES_DIR/system/.dircolors" ~
 ln -sfv "$DOTFILES_DIR/system/.profile" ~
+ln -sfv "$DOTFILES_DIR/system/.bash_profile" ~
+ln -sfv "$DOTFILES_DIR/system/.inputrc" ~
+
+ln -sfv "$DOTFILES_DIR/other/redshift.conf" ~/.config

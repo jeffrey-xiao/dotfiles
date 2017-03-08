1b2ea8cea6575eb3b86d56f0ce70d8504501e0ec
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2017-03-08 17:21:57 -0500

    Update configs and separated into folders; added tmux

--- install.sh
+++ install.sh
@@ -5,11 +5,14 @@ export DOTFILES_DIR
 
 DOTFILES_DIR=$(pwd)
 
+# Linking git options
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
 
+# Linking vim options
 ln -sfv "$DOTFILES_DIR/vim/.vimrc" ~
 
+# Linking system options
 ln -sfv "$DOTFILES_DIR/system/.bash_aliases" ~
 ln -sfv "$DOTFILES_DIR/system/.bashrc" ~
 ln -sfv "$DOTFILES_DIR/system/.dircolors" ~
@@ -17,18 +20,27 @@ ln -sfv "$DOTFILES_DIR/system/.profile" ~
 ln -sfv "$DOTFILES_DIR/system/.bash_profile" ~
 ln -sfv "$DOTFILES_DIR/system/.inputrc" ~
 
+# Linking mpd and creating a playlist folder
 mkdir -p ~/.config/mpd/playlists
-ln -sfv "$DOTFILES_DIR/other/.mpdconf" ~/.config/mpd
-ln -sfv "$DOTFILES_DIR/other/redshift.conf" ~/.config
-ln -sfv "$DOTFILES_DIR/other/.ycm_extra_conf.py" ~
+ln -sfv "$DOTFILES_DIR/mpd/.mpdconf" ~/.config/mpd
 
+# Linking ncmpcpp options
 mkdir -p ~/.ncmpcpp
-ln -sfv "$DOTFILES_DIR/other/config" ~/.ncmpcpp
+ln -sfv "$DOTFILES_DIR/ncmpcpp/config" ~/.ncmpcpp
 
+# Linking tmux options
+ln -sfv "$DOTFILES_DIR/tmux/.tmux.conf" ~
+
+# Linking sublime text package options
 mkdir -p ~/.config/sublime-text-3/Packages
 rm -rf ~/.config/sublime-text-3/Packages/User
 ln -sfnv "$DOTFILES_DIR/sublime/User" ~/.config/sublime-text-3/Packages
 
+# Linking other options
+ln -sfv "$DOTFILES_DIR/other/redshift.conf" ~/.config
+ln -sfv "$DOTFILES_DIR/other/.ycm_extra_conf.py" ~
+
+# Reinstalling all vim packages via Plug and building YouCompleteMe
 rm -rf ~/.vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -c PlugInstall -c q! -c q!

6da65b1219aefea98bc79ada72fb58681e0d6a72
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2017-05-23 10:47:28 -0700

    Update .bash_twoline_simple.sh

--- system/.bash_twoline_simple.sh
+++ system/.bash_twoline_simple.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 __twoline() {
-  # Unicode symbols
-  readonly GIT_UNCOMMITED_SYMBOL="+"
-  readonly GIT_UNSTAGED_SYMBOL="*"
-  readonly GIT_UNTRACKED_SYMBOL="?"
-  readonly GIT_NEED_PUSH_SYMBOL='⇡'
-  readonly GIT_NEED_PULL_SYMBOL='⇣'
+
+  # Git symbols
+  local GIT_UNCOMMITED_SYMBOL="+"
+  local GIT_UNSTAGED_SYMBOL="*"
+  local GIT_UNTRACKED_SYMBOL="?"
+  local GIT_NEED_PUSH_SYMBOL='⇡'
+  local GIT_NEED_PULL_SYMBOL='⇣'
 
   # Solarized colorscheme
   if [[ $(tput colors) -ge 256 ]] 2>/dev/null; then
@@ -88,40 +89,6 @@ __twoline() {
   readonly RESET="\[$(tput sgr0)\]"
   readonly BOLD="\[$(tput bold)\]"
 
-  __git_info() {
-    [ -x "$(which git)" ] || return    # git not found
-
-    local git_eng="env LANG=C git"   # force git output in English to make our work easier
-    # get current branch name or short SHA1 hash for detached head
-    local branch="$($git_eng symbolic-ref --short HEAD 2>/dev/null || $git_eng describe --tags --always 2>/dev/null)"
-    [ -n "$branch" ] || return  # git branch not found
-
-    $git_eng status &>/dev/null || return
-
-    local marks
-
-    local stat="$($git_eng status --porcelain --branch | grep '^##' | grep -o '\[.\+\]$')"
-    local all="$(git status --porcelain 2>/dev/null | wc -l | trim)"
-    local unstaged="$(git status --porcelain 2>/dev/null | grep "^ M" | wc -l | trim)"
-    local uncommited="$(git status --porcelain 2>/dev/null | grep "^M" | wc -l | trim)"
-    local untracked="$(($all-$unstaged-$uncommited))"
-    local commit="$(git rev-list HEAD --abbrev-commit --abbrev=0 -n1 2>/dev/null)"
-    local ahead="$(echo $stat | grep -o 'ahead [[:digit:]]\+' | grep -o '[[:digit:]]\+')"
-    local behind="$(echo $stat | grep -o 'behind [[:digit:]]\+' | grep -o '[[:digit:]]\+')"
-
-    [ "$untracked" -eq "0" ] || marks+=" $GIT_UNTRACKED_SYMBOL$untracked"
-    [ "$unstaged" -eq "0" ] || marks+=" $GIT_UNSTAGED_SYMBOL$unstaged"
-    [ "$uncommited" -eq "0" ] || marks+=" $GIT_UNCOMMITED_SYMBOL$uncommited"
-    [ -n "$ahead" ] && marks+=" $GIT_NEED_PUSH_SYMBOL$ahead"
-    [ -n "$behind" ] && marks+=" $GIT_NEED_PULL_SYMBOL$behind"
-
-    # print the git branch segment without a trailing newline
-    printf "$FG_YELLOW$branch$marks$FG_RESET"
-  }
-
-  trim () {
-    awk '{$1=$1};1'
-  }
   __formatted_path() {
     if [ $(id -u) -eq 0 ]; then
       printf "$FG_BLUE\w$RESET"
@@ -139,17 +106,17 @@ __twoline() {
       local FG_EXIT="$FG_RED"
     fi
 
-
     __twoline_git_info="$(__git_info)"
     __twoline_formatted_path="$(__formatted_path)"
     if [[ -z "${__twoline_git_info// }" ]]; then
       PS1="$__twoline_formatted_path\n"
       PS1+="$FG_EXIT> $RESET"
     else
-      PS1="$__twoline_formatted_path on ${__twoline_git_info}\n"
+      PS1="$__twoline_formatted_path on $FG_YELLOW${__twoline_git_info}$RESET\n"
       PS1+="$FG_EXIT> $RESET"
     fi
   }
+  source ~/.bash_git_info.sh
   PROMPT_COMMAND=ps1
 }
  __twoline

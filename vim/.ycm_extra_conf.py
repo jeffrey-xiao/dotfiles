7a2dc5c479522d9eb51a519c60d1ab921a55c01c
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2017-08-20 13:09:07 -0700

    Small fixes

--- vim/.ycm_extra_conf.py
+++ vim/.ycm_extra_conf.py
@@ -1,4 +1,17 @@
 def FlagsForFile( filename, **kwargs ):
     return {
-        'flags': ['-x', 'c++', '-Wall', '-Wextra', '-Werror' , '-std=c++11'],
+        'flags': [
+            '-x',
+            'c++',
+            '-Wall',
+            '-Wextra',
+            '-Werror',
+            '-std=c++11',
+            '-isystem',
+            '/usr/include/c++/6.4.0',
+            '-isystem',
+            '/usr/include/c++/6.4.0/x86_64-solus-linux',
+            '-isystem',
+            '/usr/include/c++/6.4.0/backward/',
+        ],
     }

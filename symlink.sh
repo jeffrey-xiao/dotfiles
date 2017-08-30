55bf7575780492d1eb0a3b78f62d263b4a4d14aa
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2017-08-29 23:25:26 -0400

    Update

--- symlink.sh
+++ symlink.sh
@@ -7,42 +7,54 @@ DOTFILES_DIR=$(pwd)
 shopt -s nullglob
 shopt -s dotglob
 
-## Linking to ~
+## Link to ~
 for folder in git vim system compton tmux rtorrent; do
   for f in $DOTFILES_DIR/$folder/*; do
     ln -sfv "$f" ~
   done
 done
 
-## Linking to ~/.$folder
+## Link to ~/.$folder
 for folder in ncmpcpp weechat templates bin; do
   # rm -rf ~/.$folder
-  mkdir -p ~/.$folder
+  mkdir -pv ~/.$folder
   for f in $DOTFILES_DIR/$folder/*; do
     ln -sfv "$f" ~/.$folder
   done
 done
 
-## Linking to .config/$folder
+## Link to .config/$folder
 for folder in mpd i3 ranger mpv fontconfig htop dunst polybar zathura; do
   # rm -rf ~/.config/$folder
-  mkdir -p ~/.config/$folder
+  mkdir -pv ~/.config/$folder
   for f in $DOTFILES_DIR/$folder/*; do
     ln -sfv "$f" ~/.config/$folder
   done
 done
 
-## Creating a playlist folder for mpd and database file
-mkdir -p ~/.config/mpd/playlists
-touch ~/.config/mpd/database
-
-## Linking other options
-mkdir -pv ~/.tmp ~/.bin ~/Downloads/complete ~/Downloads/incomplete ~/.rtorrent.sessions
-mkdir -pv ~/Pictures/screenshots
+## Link other options
 ln -sfv "$DOTFILES_DIR/other/redshift.conf" ~/.config
 ln -sfv "$DOTFILES_DIR/fonts/fonts.conf" ~/.config/fontconf
-
 for f in $DOTFILES_DIR/fonts/*.ttf; do
   ln -sfv "$f" ~/.fonts
 done
+
+## Create a playlist folder and database file for mpd
+mkdir -pv ~/.config/mpd/playlists
+touch ~/.config/mpd/database
+
+
+## Create complete, incomplete, and session folders for rtorrent
+mkdir -pv ~/.tmp ~/.bin ~/Downloads/complete ~/Downloads/incomplete ~/.rtorrent.sessions
+
+## Create screenshot folder for mpv
+mkdir -pv ~/Pictures/screenshots
+
 xrdb ~/.Xresources
+
+## Install zathura from source
+mkdir -pv ~/Documents/source
+git clone https://github.com/pwmt/zathura ~/Documents/source/zathura
+git clone https://github.com/pwmt/zathura-pdf-poppler ~/Documents/source/zathura-pdf-poppler
+cd ~/Documents/source/zathura && sudo make install
+cd ~/Documents/source/zathura-pdf-poppler && sudo make install
